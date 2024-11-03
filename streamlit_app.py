89672876bcc4112c8401a66eb2bf2aa0604f31a8
basoo1 <122086332+basoo1@users.noreply.github.com>
2024-11-03 18:53:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,38 @@
 import streamlit as st
 import leafmap.foliumap as lm
-import folium #nag issue sa atoa pakyu ka streamlit
+import folium
 
+# Create a Leafmap map instance
 m = lm.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
 
-start = (6.064593, 125.124938)
+# Define the start and end locations
+start = (6.064593, 125.124938)  # Latitude, Longitude
 end = (6.064732, 125.127561)
 
-route = folium.PolyLine(locations=[start, end], color="blue", weight=5)
+# Create a Marker for the start point
+folium.Marker(location=start, popup='Start Point').add_to(m)
 
-route.add_to(m)
+# Create a Marker for the end point
+folium.Marker(location=end, popup='End Point').add_to(m)
+
+# Add routing using Leaflet Routing Machine
+routing_script = f"""
+<script>
+var control = L.Routing.control({{
+    waypoints: [
+        L.latLng({start[0]}, {start[1]}),
+        L.latLng({end[0]}, {end[1]})
+    ],
+    routeWhileDragging: true,
+    geocoder: L.Control.Geocoder.nominatim(),
+    createMarker: function() {{ return null; }}  // Prevent markers from being created
+}}).addTo(map);
+</script>
+"""
+
+# Add the routing script to the map
+m.add_child(folium.Element(routing_script))
+
+# Render the map in Streamlit
 m.to_streamlit(height=500)
