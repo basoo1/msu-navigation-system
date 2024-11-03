44ba57f340d9758c376107f78bc169aa7b95d7be
basoo1 <122086332+basoo1@users.noreply.github.com>
2024-11-03 19:48:11 +0800

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,13 @@ import streamlit as st
 import leafmap.foliumap as lm
 import folium
 
-m = lm.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
+m = lm.Map(center = [6.064593, 125.124938], zoom = 15)    
 
 start = (6.064593, 125.124938)
 end = (6.064732, 125.127561)
 
 route = folium.PolyLine(locations=[start, end], color="blue", weight=5)
 
-route.add_to(m)
+m.add_layer(route)
 
 m.to_streamlit(height=500)
