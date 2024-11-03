40f88eaef5cfbc56431a3b0edf530262963ea6aa
basoo1 <122086332+basoo1@users.noreply.github.com>
2024-11-03 19:27:40 +0800

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,15 @@
-import leafmap
-from ipyleaflet import Map, Marker, Polyline
-import requests
+import streamlit as st
+import leafmap.foliumap as lm
+import folium
 
-# Create a map
-m = leafmap.Map(center=(14.5875, 120.9758), zoom=14)
+m = lm.Map(minimap_control=True)
+m.add_basemap("OpenTopoMap")
 
-# Define start and end points
-start = (14.5875, 120.9758)  # Starting point
-end = (14.6011, 120.9949)    # Ending point
+start = (6.064593, 125.124938)
+end = (6.064732, 125.127561)
 
-# Add markers
-start_marker = Marker(location=start, draggable=False)
-end_marker = Marker(location=end, draggable=False)
-m.add_layer(start_marker)
-m.add_layer(end_marker)
+route = folium.PolyLine(locations=[start, end], color="blue", weight=5)
 
-# Fetch route from OSRM
-response = requests.get(f'http://router.project-osrm.org/route/v1/driving/{start[1]},{start[0]};{end[1]},{end[0]}?overview=full')
-data = response.json()
+route.add_to(m)
 
-# Extract route coordinates
-route = data['routes'][0]['geometry']['coordinates']
-route_coords = [(point[1], point[0]) for point in route]
-
-# Create a polyline for the route
-route_line = Polyline(locations=route_coords, color='blue', weight=5)
-
-# Add the polyline to the map
-m.add_layer(route_line)
-
-# Display the map
-m
+m.to_streamlit(height=500)
