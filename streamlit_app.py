802c3df2947740e854b994302905c83358ce17c6
basoo1 <122086332+basoo1@users.noreply.github.com>
2024-11-03 19:16:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,17 @@
 import streamlit as st
 import leafmap.foliumap as lm
 import folium
+from ipyleaflet import rout
 
-# Create a Leafmap map instance
 m = lm.Map(minimap_control=True)
-m.add_basemap("OpenStreetMap")
+m.add_basemap("OpenTopoMap")
 
-# Define the start and end locations
-start = (6.064593, 125.124938)  # Latitude, Longitude for the start point
-end = (6.064732, 125.127561)    # Latitude, Longitude for the end point
+start = (6.064593, 125.124938)
+end = (6.064732, 125.127561)
 
-# Create markers for start and end points
-folium.Marker(location=start, popup='Start Point').add_to(m)
-folium.Marker(location=end, popup='End Point').add_to(m)
+route = folium.PolyLine(locations=[start, end], color="blue", weight=5)
 
-# Add Leaflet Routing Machine script using OSRM
-routing_script = f"""
-<script>
-var control = L.Routing.control({{
-    waypoints: [
-        L.latLng({start[0]}, {start[1]}),
-        L.latLng({end[0]}, {end[1]})
-    ],
-    routeWhileDragging: true,
-    geocoder: L.Control.Geocoder.nominatim(),
-    createMarker: function() {{ return null; }},  // Prevent markers from being created
-    router: L.Routing.osrmv1({{ serviceUrl: 'https://router.project-osrm.org/' }})  // OSRM routing service
-}}).addTo(map);
-</script>
-"""
+route.add_to(m)
 
-# Add the routing script to the map
-m.add_child(folium.Element(routing_script))
-
-# Render the map in Streamlit
-st.title("Leaflet Routing Machine with OSRM Example")
 m.to_streamlit(height=500)
+
