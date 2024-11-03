cdff8513a5d22f00187e81d9f0479fdd320606ed
basoo1 <122086332+basoo1@users.noreply.github.com>
2024-11-03 18:18:43 +0800

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,8 @@
 import streamlit as st
 import leafmap.foliumap as lm
-from ipyleaflet import Map, Marker, Polyline
 
-# Create a new Leafmap instance
+m =  lm.Map
+
 m = lm.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
-
-# Define start and end coordinates
-start = (6.064593, 125.124938)  # Latitude, Longitude
-end = (6.064732, 125.127561)
-
-# Add markers for start and end points
-start_marker = Marker(location=start, title="Start")
-end_marker = Marker(location=end, title="End")
-m.add_marker(start_marker)
-m.add_marker(end_marker)
-
-# Create a routing feature
-route = lm.Polyline(locations=[start, end], color="blue", weight=5)
-m.add_layer(route)
-
-# Display the map in Streamlit
 m.to_streamlit(height=500)
