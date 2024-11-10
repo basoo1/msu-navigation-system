3865ffa4ffa405a86a113e4f9944c3de9493093a
basoo1 <122086332+basoo1@users.noreply.github.com>
2024-11-11 01:03:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import leafmap.foliumap as lm
-import folium
+import folium as fm
 import osmnx as ox
 
-m = lm.Map(center=[6.064593, 125.124938], zoom=15)
+m = fm.Map(center=[6.064593, 125.124938], zoom=15)
 
 coords = [(125.124938, 6.064593), (125.128223, 6.068402)]
 
@@ -15,7 +14,7 @@ dest_node = ox.distance.nearest_nodes(G, X=coords[1][0], Y=coords[1][1])
 route = ox.distance.shortest_path(G, orig_node, dest_node, weight='length')
 route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
 
-folium.PolyLine(locations=route_coords, color='blue', weight=5).add_to(m)
+fm.PolyLine(locations=route_coords, color='blue', weight=5).add_to(m)
 
 # Display the map
 m.to_streamlit(height=500)
