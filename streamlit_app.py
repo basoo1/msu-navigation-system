04a75e1dc84cdc75aad3ce269dc4bba05419f8de
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-01-02 18:09:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ G = ox.graph_from_place('Mindanao State University General Santos, General Santo
 orig_node = ox.distance.nearest_nodes(G, X=coords[0][0], Y=coords[0][1])
 dest_node = ox.distance.nearest_nodes(G, X=coords[1][0], Y=coords[1][1])
 
-route = ox.distance.shortest_path(G, orig_node, dest_node, weight='length')
+route = ox.routing.shortest_path(G, orig_node, dest_node, weight='length')
 route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
 
 fm.PolyLine(locations=route_coords, color='blue', weight=5).add_to(m)
