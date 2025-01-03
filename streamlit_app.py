747e300bf0021df9337e189b8901fb6e7ae7445b
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-01-03 22:41:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,56 +3,44 @@ import folium as fm
 import osmnx as ox
 from streamlit_folium import st_folium
 from folium.plugins import Fullscreen
+from folium.plugins import MousePosition
 import json
+import utility
+
+#Streamlit Setup
 st.set_page_config(layout="wide")
-def find_closest_match(user_input, locations):
-    for location, details in locations.items():
-        # check if the user input matches the prefix of each locations
-        for name in [location] + details["aliases"]:
-            if name.lower().startswith(user_input.lower()):
-                return location
-    
-    return "No match found."
- 
-minLat, maxLat =  6.07406, 6.06009
-minLon, maxlon =  125.13257, 125.12135
 
-m = fm.Map(location=[6.064593, 125.124938], 
-           zoom_start=16,
-           max_bounds=True,
-           min_lat=minLat,
-           max_lat=maxLat,
-           min_lon=minLon,
-           max_lon=maxlon,
-           min_zoom=16
-           )
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+remove_top_margin = """
+            <style>
+            .block-container 
+            {padding-top: 0.5rem;
+            padding-bottom: 0rem;
+            padding-left: 5rem;
+            padding-right: 5rem;}
+            </style>
+            """
 
-# json file
-with open("locations.json", "r") as file:
-    locations = json.load(file)
+st.markdown(remove_top_margin, unsafe_allow_html=True)
+st.markdown(hide_st_style, unsafe_allow_html=True)
+st.container(height=None, border=None, key=None)
+
 
 x = st.text_input("Enter Location")
-
-result = find_closest_match(x, locations)
-
+with open("locations.json", "r") as file:
+    locations = json.load(file)
+result = utility.findMatch(x, locations)
 location_details = locations[result]
 location_coords = location_details["coordinates"]
-
 coords = [(125.124938, 6.064593), (location_coords)]
-
-G = ox.graph_from_place('Mindanao State University General Santos, General Santos, Philippines', network_type='all')
-
-orig_node = ox.distance.nearest_nodes(G, X=coords[0][0], Y=coords[0][1])
-dest_node = ox.distance.nearest_nodes(G, X=coords[1][0], Y=coords[1][1])
-
-route = ox.routing.shortest_path(G, orig_node, dest_node, weight='length')
-route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
-
-#draw
-fm.PolyLine(locations=route_coords, 
-            color='blue', 
-            weight=5).add_to(m)
-
+m =  utility.createMap()
+utility.addRoute(m, coords)
 Fullscreen().add_to(m)
-# display the map
+MousePosition().add_to(m)
 st_folium(m, use_container_width=True)
