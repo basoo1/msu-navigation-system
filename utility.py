313cda873b2c82f1c9cc523bfb963a448c6eea00
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-01-04 00:25:50 +0800

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,46 +1,42 @@
-import streamlit as st
 import folium as fm
 import osmnx as ox
-from streamlit_folium import st_folium
-from folium.plugins import LocateControl
-from folium.plugins import Fullscreen
+
 from folium.plugins import MousePosition
-import json
-import utility
 
-#Streamlit Setup
-st.set_page_config(layout="wide")
+def findMatch(user_input, locations):
+    for location, details in locations.items():
+        # check if the user input matches the prefix of each locations
+        for name in [location] + details["aliases"]:
+            if name.lower().startswith(user_input.lower()):
+                return location
+    
+    return "No match found."
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-remove_top_margin = """
-            <style>
-            .block-container 
-            {padding-top: 0.5rem;
-            padding-bottom: 0rem;
-            padding-left: 5rem;
-            padding-right: 5rem;}
-            </style>
-            """
+def createMap():
+    minLat, maxLat = 6.07406, 6.06009
+    minLon, maxLon = 125.13257, 125.12135
 
-st.markdown(remove_top_margin, unsafe_allow_html=True)
-st.markdown(hide_st_style, unsafe_allow_html=True)
-st.container(height=None, border=None, key=None)
+    m = fm.Map(location=[6.064593, 125.124938], 
+               zoom_start=16,
+               max_bounds=True,
+               min_lat=minLat,
+               max_lat=maxLat,
+               min_lon=minLon,
+               max_lon=maxLon,
+               min_zoom=16)
+    return m
 
-x = st.text_input("Enter Location")
-with open("locations.json", "r") as file:
-    locations = json.load(file)
-result = utility.findMatch(x, locations)
-location_details = locations[result]
-location_coords = location_details["coordinates"]
-coords = [(6.067531, 125.126034), (location_coords)]
-m =  utility.createMap()
-utility.addRoute(m, coords)
-LocateControl(auto_start=True).add_to(m)
-Fullscreen().add_to(m)
-st_folium(m, use_container_width=True)
+def addRoute(m, coords):
+    G = ox.graph_from_place('Mindanao State University General Santos, General Santos, Philippines', network_type='all')
+
+    orig_node = ox.distance.nearest_nodes(G, X=coords[0][1], Y=coords[0][0])
+    dest_node = ox.distance.nearest_nodes(G, X=coords[1][1], Y=coords[1][0])
+
+    route = ox.routing.shortest_path(G, orig_node, dest_node, weight='length')
+    route_coords = [(G.nodes[node]['y'], G.nodes[node]['x']) for node in route]
+
+    fm.PolyLine(locations=route_coords, color='blue', weight=5).add_to(m)
+
+#using mouse for testing
+def getUserLocation(m):
+    MousePosition().add_to(m)
