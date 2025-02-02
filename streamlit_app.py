db4f885432065e6e5251e10b40cc32890ed5abd0
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-02-02 13:08:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import folium as fm
 import osmnx as ox
+from folium.plugins import LocateControl as lc
 from streamlit_folium import st_folium
 from streamlit_js_eval import get_geolocation
 import json
@@ -38,26 +39,35 @@ st.markdown(custom_margin, unsafe_allow_html=True)
 st.markdown(hide_st_style, unsafe_allow_html=True)
 st.markdown(removerandomelement, unsafe_allow_html=True)
 
-x = st.text_input("Enter Location")
+if 'map' not in st.session_state:
+    st.session_state['map'] = utility.createMap(lat=0, lng=0)
+
+x = st.text_input('Enter Location')
 
 m = utility.createMap(lat=0, lng=0)
 
 if x:
    with open("locations.json", "r") as file:
       locations = json.load(file)
+
    result = utility.findMatch(x, locations)
+
+   local_coords = get_geolocation()
    location_details = locations[result]
    location_coords = location_details["coordinates"]
 
-   local_coords = get_geolocation()
-
    if local_coords:
+      
       local_lat = local_coords['coords']['latitude'] 
       local_lng = local_coords['coords']['longitude']
 
       coords = [(local_lat, local_lng), (location_coords)]
 
-      m = utility.createMap(lat=local_lat, lng=local_lng)
-      r = utility.addRoute(m, coords)
+      st.session_state["map"] = utility.createMap(lat=local_lat, lng=local_lng)
+      utility.addRoute(st.session_state["map"], coords)
+      fm.Marker(location=(local_lat, local_lng), icon=fm.Icon(color="blue")).add_to(st.session_state['map'])
+      fm.Marker(location=(location_coords), icon=fm.Icon(color="red")).add_to(st.session_state['map'])
+   else:
+      st.error('Unable to obtain device location')
 
-st_folium(m, use_container_width=True, height=500)
+st_folium(st.session_state["map"], use_container_width=True, height=500)
