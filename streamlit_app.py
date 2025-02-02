9a83ffa677f8df8a7ef17d0aec157e2403da016c
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-02-02 13:22:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,8 @@ if x:
 
    result = utility.findMatch(x, locations)
 
-   x = None
+   x = st.empty
+   x = st.text_input('Enter Location')
 
    location_details = locations[result]
    location_coords = location_details["coordinates"]
