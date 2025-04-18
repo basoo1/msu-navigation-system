4f2ad917e2455b655d9b80c91ef602402b01d5f1
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-04-18 12:25:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from streamlit_js_eval import get_geolocation
 import json
 import utility
 
-# this fixes wide issue
+# stremalit setup
 st.set_page_config(layout="wide")
 
 hide_st_style = """
@@ -20,8 +20,8 @@ hide_st_style = """
 custom_margin = """
          <style>
          .block-container 
-         {padding-top: 0rem;
-         padding-bottom: 0rem;
+         {padding-top: 1.3rem;
+         padding-bottom: 1.3em;
          padding-left: 0.5rem;
          padding-right: 0.5rem;}
          .element-container 
@@ -36,9 +36,9 @@ removerandomelement = """
          """
 
 st.markdown(custom_margin, unsafe_allow_html=True)
-st.markdown(hide_st_style, unsafe_allow_html=True)
 st.markdown(removerandomelement, unsafe_allow_html=True)
 
+# preload map
 if 'map' not in st.session_state:
     st.session_state['map'] = utility.createMap(lat=0, lng=0)
 
@@ -46,29 +46,38 @@ x = st.text_input('Enter Location')
 
 m = utility.createMap(lat=0, lng=0)
 
-local_coords = get_geolocation()
-
+#check locatations file
 if x:
    with open("locations.json", "r") as file:
-      locations = json.load(file)
+      locations = json.load(file)   
 
    result = utility.findMatch(x, locations)
 
    location_details = locations[result]
    location_coords = location_details["coordinates"]
 
-   if local_coords:
+   if 'local_coords' not in st.session_state:
+      geolocation = get_geolocation()
+      if geolocation:
+         st.session_state['local_coords'] = geolocation
+      else:
+         st.error("Please wait while we get your device's location.")
+   else:
+      geolocation = st.session_state['local_coords']
+
+#check location
+   if geolocation:
       
-      local_lat = local_coords['coords']['latitude'] 
-      local_lng = local_coords['coords']['longitude']
+      local_lat = geolocation['coords']['latitude'] 
+      local_lng = geolocation['coords']['longitude']
 
       coords = [(local_lat, local_lng), (location_coords)]
 
+      #main 
       st.session_state["map"] = utility.createMap(lat=local_lat, lng=local_lng)
       utility.addRoute(st.session_state["map"], coords)
+
       fm.Marker(location=(local_lat, local_lng), icon=fm.Icon(color="blue")).add_to(st.session_state['map'])
       fm.Marker(location=(location_coords), icon=fm.Icon(color="red")).add_to(st.session_state['map'])
-   else:
-      st.error('Unable to obtain device location')
 
-st_folium(st.session_state["map"], use_container_width=True, height=500)
+st_folium(st.session_state["map"], use_container_width=True, height=500, returned_objects=[])
