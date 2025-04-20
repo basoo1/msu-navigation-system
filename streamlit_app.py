9748b3623000372a7be161cb35be45eef27e18e2
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-04-21 06:43:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,35 @@ import folium as fm
 import osmnx as ox
 import json
 import utility
+import streamlit.components.v1 as components
 from streamlit_folium import st_folium
 from streamlit_js_eval import get_geolocation
 
 # streamlit setup
 st.set_page_config(layout="wide")
+markerFG = fm.FeatureGroup(name='Markers')
 
 move_search_bar_up = """
    <style>
-   .st-emotion-cache-ocqkz7 {
+   .stSelectbox {
    margin-top: -4.5em;
+   }
+   <style>
+"""
+
+move_sidebar_collapse_button = """
+   <style>
+   [data-testid="stSidebarCollapsedControl"] {
+      margin-top: 19rem;
+      margin-left: -1.5rem;
+   }
+   </style>
+"""
+
+move_sidecontrol = """
+   <style>
+   stBaseButton-headerNoPadding {
+   margin-bottom: -4.5em;
    }
    <style>
 """
@@ -46,6 +65,7 @@ st.markdown(move_search_bar_up, unsafe_allow_html=True)
 st.markdown(custom_margin, unsafe_allow_html=True)
 st.markdown(remove_getLocation, unsafe_allow_html=True)
 st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown(move_sidebar_collapse_button, unsafe_allow_html=True)
 
 with open("locations.json", "r") as file:
    locations = json.load(file)   
@@ -59,30 +79,15 @@ for location, details in locations.items():
    search_options.append(location)
    search_map[location] = {"location": location, "office": None}
 
-#add options for offices
-   for office in details["offices"]:
-      display_name = f"{location} ({office})"
-      search_options.append(display_name)
-      search_map[display_name] = {"location": location, "office": office}
-
 #preload map
 if 'map' not in st.session_state:
    st.session_state['map'] = utility.createMap(lat=0, lng=0)
 
 
-col1, col2 = st.columns([5, 1])
+selected_option = st.selectbox('Search', search_options)
 
-#select box in first column
-search_options.sort()
-with col1:
-   selected_option = st.selectbox('', search_options)
-
-#rerun button in second column
-with col2:
-   update_button = st.button("Update Location")
-
-#create map
-m = utility.createMap(lat=0, lng=0)
+#create sidebar
+sidebar = st.sidebar
 
 if selected_option:
    location_info = search_map.get(selected_option)
@@ -92,11 +97,20 @@ if selected_option:
         
       location_details = locations[result]
       location_coords = location_details["coordinates"]
+      
+      #display
+      sidebar.header(result)
+      if location_details["offices"]:
+          sidebar.subheader("Offices:")
+          for office in location_details["offices"]:
+              sidebar.write(f"• {office}")
+      else:
+          sidebar.write("No offices found in this building.")
 
       if 'local_coords' not in st.session_state:
          geolocation = get_geolocation()
          if geolocation:
-               st.session_state['local_coords'] = geolocation
+            st.session_state['local_coords'] = geolocation
       else:
          geolocation = st.session_state['local_coords']
 
@@ -113,10 +127,16 @@ if selected_option:
 
          fm.Marker(location=(local_lat, local_lng), 
                   icon=fm.Icon(color="blue")
-                  ).add_to(st.session_state['map'])
+                  ).add_to(markerFG)
             
          fm.Marker(location=(location_coords), 
-                   icon=fm.Icon(color="red")
-                   ).add_to(st.session_state['map'])
+                  icon=fm.Icon(color="red"),
+                  popup=selected_option
+                  ).add_to(markerFG)
+         markerFG.add_to(st.session_state['map'])
 
-st_folium(st.session_state["map"], use_container_width=True, height=500, returned_objects=[])
+st_folium(st.session_state["map"], 
+          use_container_width=True, 
+          height=500,
+          feature_group_to_add=markerFG,
+          returned_objects=[])
