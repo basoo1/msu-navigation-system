aa5658d0f964e0e4d1199ed0eb25fbdd365b1f63
basoo1 <122086332+basoo1@users.noreply.github.com>
2025-04-20 16:32:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,5 +88,4 @@ if x:
       fm.Marker(location=(local_lat, local_lng), icon=fm.Icon(color="blue")).add_to(st.session_state['map'])
       fm.Marker(location=(location_coords), icon=fm.Icon(color="red")).add_to(st.session_state['map'])
        
-st.write("Polyline points:", polyline_coords)
 st_folium(st.session_state["map"], use_container_width=True, height=500, returned_objects=[])
